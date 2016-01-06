bdf7536fd8c0486d0b2369f08c514218fec01e60
Raphael Luiz S. de Oliveira Moraes <vntramo@venturus.local>
2016-01-06 16:44:47 -0200

    Initial commit

---
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -0,0 +1,39 @@
+import {Component} from 'angular2/core';
+
+interface Hero {
+    id: number;
+    name: string;
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>
+    <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    </div>
+    `
+})
+
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'windstorm'
+    }
+}
